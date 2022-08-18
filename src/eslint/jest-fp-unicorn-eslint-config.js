a5a19b532b70304756962e5cfaeca4ae03319bb7
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2022-08-18 14:06:57 +0100

    bump to latest moar + using the moar-stricter and moar-test... on the path to become agnostic to that

--- src/eslint/jest-fp-unicorn-eslint-config.js
+++ src/eslint/jest-fp-unicorn-eslint-config.js
@@ -11,33 +11,8 @@ module.exports = {
     {
       files: ['**/src/**'],
       extends: [
-        'plugin:fp/recommended',
-        'plugin:unicorn/recommended',
+        'node-moar-stricter',
       ],
-      rules: {
-        'fp/no-unused-expression': 'off',
-        'fp/no-let': 'warn',
-        'fp/no-nil': 'off',
-        'fp/no-rest-parameters': 'off',
-        'fp/no-mutation': ['error', {
-          commonjs: true,
-          allowThis: true,
-          exceptions: [],
-        }],
-        'security/detect-non-literal-fs-filename': 'off',
-        'security/detect-object-injection': 'off',
-        'unicorn/prefer-module': 'off',
-        'unicorn/escape-case': 'warn',
-        'unicorn/import-style': 'off',
-        'unicorn/prevent-abbreviations': 'warn',
-        'unicorn/no-null': 'warn',
-        'unicorn/no-array-for-each': 'off', // does not sounds right to me
-        'unicorn/no-array-reduce': 'warn',
-        'unicorn/no-array-callback-reference': 'off', // does not sounds right to me
-        'unicorn/no-fn-reference-in-iterator': 'off', // does not sounds right to me
-        'unicorn/no-process-exit': 'off', // eslint already has it
-        'unicorn/prefer-node-protocol': 'off',
-      },
     },
     {
       files: [
@@ -48,22 +23,8 @@ module.exports = {
         'jest.config.*.js',
       ],
       extends: [
-        'plugin:jest/recommended',
+        'node-moar-test',
       ],
-      rules: {
-        'max-lines': ['warn', 200],
-        'max-nested-callbacks': ['warn', 3],
-        'no-console': 'off',
-        'no-sync': 'off',
-        'jest/expect-expect': 'off',
-        'jest/no-done-callback': 'off',
-        'jest/no-conditional-expect': 'off',
-        'node/no-unpublished-require': 'off',
-        'node/no-extraneous-require': 'off',
-        'promise/no-callback-in-promise': 'off',
-        'security/detect-non-literal-fs-filename': 'off',
-        'sonarjs/no-duplicate-string': 'off',
-      },
     },
   ],
 }
